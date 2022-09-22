8c1f0db6f1e0cf34fc8b1a3140d423230438a48a
luismp <luis.aaron.mp17@gmail.com>
2022-09-22 12:25:16 -0500

    added endpoint to delete a particular user delete(/user/:name)

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const { requestLogger } = require('../utils/middleware')
 app.use(bodyParser())
 if (process.env.NODE_ENV === 'dev') app.use(requestLogger)
 
-const Users = []
+let Users = []
 
 router.get('/', async (ctx) => {
   ctx.body = `Seu servidor esta rodando em http://localhost:${PORT}`
@@ -45,6 +45,19 @@ router.get('/user/:name', async (ctx, next) => {
     ctx.response.status = 200
   } else {
     ctx.body = { message: 'User not found' }
+    ctx.response.status = 404
+  }
+})
+
+router.delete('/user/:name', async (ctx) => {
+  const name = ctx.params.name
+  const user = Users.find(user => user.name === name)
+
+  if (user) {
+    ctx.body = user
+    Users = Users.filter(user => user.name !== name)
+    ctx.response.status = 200
+  } else {
     ctx.response.status = 404
   }
 })
